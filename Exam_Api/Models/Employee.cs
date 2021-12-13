ee4f1270c42084f9ddf312dd913e4f6ddd67c2c2
vuong huu phu <phudzazxc@gmail.com>
2021-12-13 09:51:28 +0700

    first commit

---
+++ Exam_Api/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Exam_Api.Models
+{
+    public class Employee
+    {
+        public int Studentid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Exam_Api/Models/Exam_apiContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Exam_Api.Models
+{
+    public partial class Exam_apiContext : DbContext
+    {
+
+        public Exam_apiContext(DbContextOptions<Exam_apiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Studentid)
+                    .HasName("PK__Employee__32CE55B1ED4ADF2F");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Email).HasMaxLength(255);
+
+                entity.Property(e => e.FirstName).HasMaxLength(255);
+
+                entity.Property(e => e.LastName).HasMaxLength(255);
+
+                entity.Property(e => e.PhoneNumber).HasMaxLength(10);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
